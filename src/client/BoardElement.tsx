a7dd5d63ddd2976b1784e5585409c5fce3435493
Justin Sun <jsun103@uw.edu>
2026-03-06 09:36:37 -0800

    react-pan-pinch-zoom added with warnings

--- src/client/BoardElement.tsx
+++ src/client/BoardElement.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { Board, BoardPiece, PositionedTamerlanePiece } from "../common/Board";
 
 export interface BoardElementProperties {
@@ -8,6 +9,11 @@ export interface BoardElementProperties {
 export default function BoardElement(props: BoardElementProperties) {
     return (
         <>
+    <TransformWrapper>
+      <TransformComponent>
+        <img src="image.jpg" alt="test" />
+      </TransformComponent>
+    </TransformWrapper>
         </>
     )
 }
