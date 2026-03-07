ba2b16d98595541d9a2e33b8f1b001a6ed53ad35
Justin Sun <jsun103@uw.edu>
2026-03-06 20:30:43 -0800

    indicator markers

--- src/client/GamePage.tsx
+++ src/client/GamePage.tsx
@@ -1,25 +1,32 @@
-import { Component, useReducer, useRef } from "react";
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import TamerlanePieces from "./TamerlanePieces";
-import { Board } from "../common/Board";
 import { ClientInstance } from "./ClientInstance";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import TamerlaneGrid from "./TamerlaneGrid";
+import PossibleMoves from "./PossibleMoves";
+import { BoardPosition } from "../common/Position";
 
 interface GamePageProperties {
     readonly instance: ClientInstance | null;
 }
 
 export default function GamePage(props: GamePageProperties) {
+    useState();
     return (
         <>
             <TransformWrapper>
-                <TransformComponent wrapperClass="viewport"
-                        contentClass="canvas">
+                <TransformComponent 
+                    wrapperClass="viewport"
+                    contentClass="canvas"
+                >
                     <div className="board">
                         <TamerlaneGrid />
                         <TamerlanePieces 
                             pieces={props.instance !== null ? [...props.instance.getPieces()] : []} 
+                        />
+                        <PossibleMoves 
+                            client={props.instance} 
+                            possibleMoves={props.instance !== null ? [...props.instance.getMovesFor(new BoardPosition(2, 2))] : []}
                         />
                     </div>
                 </TransformComponent>
---
+++ src/client/PossibleMoves.tsx
@@ -0,0 +1,37 @@
+import { Board, BoardPiece, PositionedTamerlanePiece } from "../common/Board";
+import getImgSrc from "./PieceImgLibrary";
+import React, { useState } from "react";
+import PositionedCSSProperties from "./PositionedCSSProperties";
+
+import { getKey, makePos } from "./Utilities";
+import { MoveUnion } from "../common/Move";
+import { ClientInstance } from "./ClientInstance";
+
+export interface PossibleMovesProperties {
+    readonly client: ClientInstance | null;
+    readonly possibleMoves: MoveUnion[];
+}
+
+export default function PossibleMoves(props: PossibleMovesProperties) {
+    console.log(props.possibleMoves)
+    return (
+        <>
+            {
+                props.possibleMoves.map(
+                    move => {
+                        return (
+                            <button 
+                                className="indicator-button" 
+                                style={makePos(move.end)} 
+                                key={getKey(move.end)}>
+                                <img
+                                    className="indicator-icon" 
+                                    src="res/IndicatorEmpty.png"/>
+                            </button>
+                        )
+                    }
+                )
+            }
+        </>
+    )
+}
