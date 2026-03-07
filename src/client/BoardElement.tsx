9e39f729b12a0abbf87023e6492ebba86b15e37c
Justin Sun <jsun103@uw.edu>
2026-03-06 16:02:21 -0800

    board tiles

--- src/client/BoardElement.tsx
+++ src/client/BoardElement.tsx
@@ -6,20 +6,41 @@ import React from "react";
 import PositionedCSSProperties from "./PositionedCSSProperties";
 
 import './BoardElement.css'
+import { BoardPosition } from "../common/Position";
 
 export interface BoardElementProperties {
     readonly pieces: PositionedTamerlanePiece[];
 }
+
+const tileSize: number = 64;
+
 export default function BoardElement(props: BoardElementProperties) {
     return (
         <TransformWrapper>
             <TransformComponent wrapperClass="viewport"
                     contentClass="canvas">
                 <div className="board">
+                    {
+                        // Create the tiles as divs by mapping a list of all possible positions to tiles
+                        BoardPosition.allPositions.map(
+                            pos => {
+                                const style: PositionedCSSProperties = {
+                                    "--x": `${(pos.file + 1) * tileSize}px`,
+                                    "--y": `${pos.rank * tileSize}px`,
+                                }
+                                return (
+                                    <div className={`tile-${((pos.file + pos.rank) % 2) === 0 ? "black" : "white"}`}
+                                        style={style}>
+
+                                    </div>
+                                )
+                            }
+                        )
+                    }
                     {props.pieces.map(piece => {
                         const style: PositionedCSSProperties = {
-                            "--x": `${(piece.position.file + 2) * 64}px`,
-                            "--y": `${piece.position.rank * 64}px`,
+                            "--x": `${(piece.position.file + 1) * tileSize}px`,
+                            "--y": `${piece.position.rank * tileSize}px`,
                         }
                         return (
                             <button className="board-piece" style={style}>
