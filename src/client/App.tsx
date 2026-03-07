fb75a8564857c14ef6303b61eb06158d31e07422
Justin Sun <jsun103@uw.edu>
2026-03-06 23:19:44 -0800

    routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -5,6 +5,7 @@ import TamerlanePieces from './TamerlanePieces'
 import GamePage from './GamePage'
 import { ClientInstance } from './ClientInstance'
 import { PlayerENUM } from '../common/Player'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 	const client = useRef<ClientInstance | null>(new ClientInstance(PlayerENUM.Black, 0, 0))
@@ -12,9 +13,17 @@ function App() {
 
 
 	return (
-		<>
-			<GamePage instance={client.current}></GamePage>
-		</>
+		<Routes>
+			<Route 
+				path="/"
+			/>
+			<Route 
+				path="/instance"
+				element={
+					<GamePage instance={client.current}></GamePage>
+				}
+			/>
+		</Routes>
 	)
 }
 
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import React from 'react'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
